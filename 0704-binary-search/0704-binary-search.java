9caa9bf3043bc16d2448a15bec9c5aa08c8b2996
Dheeraj kumar <100116720+dheeraj1kumar@users.noreply.github.com>
2023-01-11 21:16:09 +0530

    Time: 0 ms (100.00%), Space: 43.4 MB (56.16%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -1,11 +1,33 @@
 //linera
 
 class Solution {
-    public int search(int[] n, int target) {
-        for(int i=0;i<n.length;i++){
-            if(n[i]==target)
-                return i;
+//     public int search(int[] n, int target) {
+//         for(int i=0;i<n.length;i++){
+//             if(n[i]==target)
+//                 return i;
+//         }
+//         return -1;
+//     }
+// }
+
+
+
+ public static int search(int n[],int k){
+        int start=0;
+        int end = n.length-1;
+        while(start<=end){
+            int mid=(end+start)/2;
+            if(n[mid]==k){
+                return mid;
+
+            }
+            else if(n[mid]<k){
+                start=mid+1;
+            }
+            else{
+              end=  mid-1;
+            }
         }
         return -1;
-    }
-}
+
+    }}

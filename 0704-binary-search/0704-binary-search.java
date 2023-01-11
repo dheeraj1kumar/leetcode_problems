4f3eb6473fd4c0033685877de4bb18dfa5cf6b98
Dheeraj kumar <100116720+dheeraj1kumar@users.noreply.github.com>
2023-01-11 21:10:02 +0530

    Time: 1 ms (15.55%), Space: 42.8 MB (92.00%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int search(int[] n, int target) {
+        for(int i=0;i<n.length;i++){
+            if(n[i]==target)
+                return i;
+        }
+        return -1;
+    }
+}

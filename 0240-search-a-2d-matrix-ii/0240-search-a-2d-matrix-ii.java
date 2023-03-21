cb72b549a3fa635baf08f91428d4bc1d80233caf
Dheeraj kumar <100116720+dheeraj1kumar@users.noreply.github.com>
2023-03-21 22:48:40 +0530

    Time: 10 ms (26.73%), Space: 48.6 MB (20.10%) - LeetHub

---
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean searchMatrix(int[][] m, int target) {
+        for(int i=0;i<m.length;i++){
+for(int j=0;j<m[i].length;j++){
+    if(m[i][j]==target){
+return true;}
+}}
+        return false;
+    }
+}

851a013d57ef4d395a7fe08c2e25afa290a72ea3
Dheeraj kumar <100116720+dheeraj1kumar@users.noreply.github.com>
2023-03-30 09:49:59 +0530

    Added solution - LeetHub

---
+++ Search an element in sorted and rotated array - GFG/search-an-element-in-sorted-and-rotated-array.java
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+import java.io.*;
+import java.util.*; 
+
+class GFG{
+    public static void main(String args[]) throws IOException { 
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while(t > 0){
+        	int n = sc.nextInt();
+        	int[] array = new int[n];
+        	for(int i=0; i<n; i++)
+        	{
+        		array[i] = sc.nextInt();
+        	}
+        	int target = sc.nextInt();
+
+            Solution ob = new Solution();
+			System.out.println(ob.Search(array,target));
+            t--;
+        }
+    } 
+} 
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution 
+{ 
+    static int Search(int a[], int target)
+	{
+ for(int i=0;i<a.length;i++){
+     if(a[i]==target){
+         return i;
+     }
+ }
+	  return -1;
+	}
+} 
+

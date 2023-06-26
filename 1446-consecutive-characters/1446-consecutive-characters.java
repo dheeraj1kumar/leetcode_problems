53ab6935671d3b13cdde49492ae027204d1917fe
Dheeraj kumar <100116720+dheeraj1kumar@users.noreply.github.com>
2023-06-26 19:51:10 +0530

    Time: 1 ms (100.00%), Space: 40.6 MB (99.71%) - LeetHub

---
+++ 1446-consecutive-characters/1446-consecutive-characters.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxPower(String s) {
+      int count = 0,maxCount = 0;
+        char previous = ' ';
+        for (int i=0;i<s.length();i++) {
+            char c = s.charAt(i);
+            if (c==previous) 
+                count++;
+            else{
+                count=1;
+                previous=c;
+            }
+            maxCount=Math.max(maxCount,count);
+        }
+        return maxCount;     
+    }
+}

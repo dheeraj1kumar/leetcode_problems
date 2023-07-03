14dbad6e867a45887415c9dba83b86c3a56e1e26
Dheeraj kumar <100116720+dheeraj1kumar@users.noreply.github.com>
2023-07-03 16:29:10 +0530

    Time: 6 ms (18.56%), Space: 41.5 MB (83.85%) - LeetHub

---
+++ 0859-buddy-strings/0859-buddy-strings.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean buddyStrings(String s, String goal) {
+      if(s.length() != goal.length()) return false;
+        int diff1 = - 1, diff2 = - 1;
+        Set<Character> set = new HashSet<>();
+        for(int i = 0; i < s.length(); i ++) {
+            if(s.charAt(i) != goal.charAt(i)) {
+                if(diff1 == - 1)
+                diff1 = i;
+                else if(diff2 == - 1)
+                diff2 = i;
+                else 
+                return false;
+            }
+            set.add(s.charAt(i));
+        }
+        if(diff1 != - 1 && diff2 != - 1)
+        return s.charAt(diff1) == goal.charAt(diff2) && s.charAt(diff2) == goal.charAt(diff1);
+        if(diff1 != - 1) return false;
+        return set.size() < s.length();   
+    }
+}

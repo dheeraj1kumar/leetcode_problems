da249ff6819169aff9ebe843da29432749bf90d2
Dheeraj kumar <100116720+dheeraj1kumar@users.noreply.github.com>
2023-11-10 22:20:30 +0530

    Time: 11 ms (41.83%), Space: 44.1 MB (66.93%) - LeetHub

--- 1759-count-number-of-homogenous-substrings/1759-count-number-of-homogenous-substrings.java
+++ 1759-count-number-of-homogenous-substrings/1759-count-number-of-homogenous-substrings.java
@@ -1,18 +1,37 @@
+// class Solution {
+//     public int countHomogenous(String s) {
+//        if(s.length()<=1) return s.length();
+//         int[] dp=new int[s.length()];
+//         Arrays.fill(dp,1);
+//         for(int i=1; i<dp.length ; i++){
+//             if(s.charAt(i)==s.charAt(i-1)){
+//                 dp[i]=dp[i-1]+1;
+//             }
+//         }
+//         int sum=0;
+//         for(int i=0; i<dp.length ; i++){
+//             sum+=dp[i];
+//             sum%=1000000007;
+//         }
+//         return sum; 
+//     }
+// }
+
 class Solution {
     public int countHomogenous(String s) {
-       if(s.length()<=1) return s.length();
-        int[] dp=new int[s.length()];
-        Arrays.fill(dp,1);
-        for(int i=1; i<dp.length ; i++){
-            if(s.charAt(i)==s.charAt(i-1)){
-                dp[i]=dp[i-1]+1;
-            }
+        int prev = 1;
+    int sum = 1;
+    int m = 1000000007;
+    for(int i = 1; i < s.length(); i++)
+    {
+        int curr = 1;
+        if(s.charAt(i) == s.charAt(i-1))
+        {
+            curr = (curr + prev) % m;
         }
-        int sum=0;
-        for(int i=0; i<dp.length ; i++){
-            sum+=dp[i];
-            sum%=1000000007;
-        }
-        return sum; 
+        sum = (sum + curr) % m;
+        prev = curr;
+    }
+    return sum;
     }
 }

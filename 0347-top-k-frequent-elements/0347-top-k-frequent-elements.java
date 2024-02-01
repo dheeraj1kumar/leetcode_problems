820b70bb4784858a28d8fbe537cdf091113e5ad3
Dheeraj kumar <100116720+dheeraj1kumar@users.noreply.github.com>
2024-02-01 16:13:44 +0530

    Time: 14 ms (63.57%), Space: 48.1 MB (62.33%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,17 +1,30 @@
+/*Simple solution, HashMap stores the elements and the count while allowing a constant time lookup.
+The PriorityQueue then accepts the key value pair from the HashMap and the PriorityQueue then places a priority on the highest value from the key value pair.
+
+So the second loop inserts the key value pair, the final while loop then takes the priority of each key value pair, the highest value within the priority queue will come first, removes that element, so the next highest value will be ready for polling, then inserts each one into an array.*/
+
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
         
-      HashMap<Integer,Integer> map=new HashMap<>();// a hash map to store number as key and frquency as value
-        for(int i=0;i<nums.length;i++){
-            map.put(nums[i],map.getOrDefault(nums[i],0)+1); // getOrDefault gives a frequency in case key already exist else 0 as default
-        }
-    Queue<Integer> pq = new PriorityQueue<>((a,b) ->(map.get(a)-map.get(b)));  // PQ initialized to sort based on frequency at insertion.
+        for (int i: nums)
+            map.put(i, map.getOrDefault(i, 0) + 1);
         
-        for(int i : map.keySet()){    // keyset to get all nums from map keys
-            pq.add(i);
-            if(pq.size()>k) pq.poll(); // checking if we reached Kth position, then delete the first value(value with least frequency yet encountered)
+        PriorityQueue<Pair<Integer, Integer>> queue = new PriorityQueue<>(
+            (a, b) -> b.getValue() - a.getValue());
+        
+        for (int key: map.keySet()){
+            queue.offer(new Pair<Integer, Integer>(key, map.get(key)));
         }
-        return pq.stream().mapToInt(Integer::intValue).toArray(); // using streams and map api to get primitive values from Integer object and return as array,  upvote if you like this approach :)
+        
+        int[] ans = new int[k];
+        int idx = 0;
+        
+        while (k > 0){
+            ans[idx++] = queue.poll().getKey();
+            k--;
+        }
+        
+        return ans;
     }
 }
-

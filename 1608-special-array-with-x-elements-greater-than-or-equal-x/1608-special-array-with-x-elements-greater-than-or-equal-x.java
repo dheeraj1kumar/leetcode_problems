5adfbeddc9ff91401d60d7e45bf2dca6d647074d
Dheeraj kumar <100116720+dheeraj1kumar@users.noreply.github.com>
2024-05-27 23:26:14 +0530

    Time: 2 ms (47.70%), Space: 41.5 MB (21.41%) - LeetHub

--- 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.java
+++ 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.java
@@ -1,20 +1,55 @@
+// class Solution {
+//     public int specialArray(int[] nums) {
+//         int n= nums.length;
+        
+//         for(int i=1;i<1000;i++){
+//             int c=0;
+//             for(int j=0;j<n;j++){
+//                 if(nums[j]>=i){
+//                     c++;
+//                                }
+//             }
+//            if(c==i){
+//                return i;
+//            }
+            
+//         }
+//         return -1;
+//     }
+// }
+
+
+
 class Solution {
     public int specialArray(int[] nums) {
-        int n= nums.length;
-        
-        for(int i=1;i<1000;i++){
-            int c=0;
-            for(int j=0;j<n;j++){
-                if(nums[j]>=i){
-                    c++;
-                               }
+        Arrays.sort(nums);
+        int n = nums.length;
+
+        for (int x = 0; x <= n; x++) {
+            int i = lowerBound(nums, x);
+
+            if (n - i == x) {
+                return x;
             }
-           if(c==i){
-               return i;
-           }
-            
         }
+
         return -1;
+    }
+
+    // Custom implementation of lower bound
+    private int lowerBound(int[] nums, int x) {
+        int left = 0;
+        int right = nums.length;
+        
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] >= x) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
     }
 }
 
